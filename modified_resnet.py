e882c0bc75b0f81681b6becd49582297fd9366e3
aravind <aravind.nujella@gmail.com>
2018-09-21 08:10:49 +0530

    eod commit

--- modified_resnet.py
+++ modified_resnet.py
@@ -25,18 +25,25 @@ class split_conv(nn.Module):
         self.copy_conv = nn.Conv2d(cur_in, d_out, *args, **kwargs)
 
     def forward(self, x):
-        ignore = self.ignore_conv(x)
-        copy = self.copy_conv(x)
-        return torch.cat([ignore, copy], 1)
+        ig = self.ignore_conv(x)
+        cp = self.copy_conv(x)
+        return torch.cat([ig, cp], 1)
+
 
 class split_bn(nn.Module):
+
     def __init__(self, cur_in, d_in, *args, **kwargs):
         super(split_bn, self).__init__()
         self.ignore_bn = nn.BatchNorm2d(cur_in, *args, **kwargs)
-        self.copy_bn = None
+        self.copy_bn = nn.BatchNorm2d(d_in, *args, **kwargs)
+        self.cur_in = cur_in
+        # self.d_in = d_in
+
     def forward(self, x):
-        ig, cp = x
-        return self.ignore_bn(ig), cp
+        ig, cp = x[:, :self.cur_in, :, :], x[:, self.cur_in:, :, :]
+        ig, cp = self.ignore_bn(ig), self.copy_bn(cp)
+        return torch.cat([ig, cp], 1)
+
 
 class Bottleneck(nn.Module):
     expansion = 4
@@ -44,12 +51,12 @@ class Bottleneck(nn.Module):
     def __init__(self, inplanes, planes, d_out, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
         self.conv1 = split_conv(inplanes, planes, d_out, kernel_size=1)
-        self.bn1 = nn.BatchNorm2d(planes + d_out)
+        self.bn1 = split_bn(planes, d_out)
         self.conv2 = split_conv(planes, planes, d_out, kernel_size=3, stride=stride,
                                 padding=1)
-        self.bn2 = nn.BatchNorm2d(planes + d_out)
+        self.bn2 = split_bn(planes, d_out)
         self.conv3 = split_conv(planes, planes * self.expansion, d_out * self.expansion, kernel_size=1)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion + d_out * self.expansion)
+        self.bn3 = split_bn(planes * self.expansion, d_out * self.expansion)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -82,31 +89,26 @@ class ModNet(nn.Module):
     def __init__(self, block, layers, d_in):
         self.inplanes = 64 + d_in
         super(ModNet, self).__init__()
-        self.conv1 = nn.Conv2d(3 + 1, self.inplanes, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(self.inplanes)
+        self.conv1 = split_conv(3 + 1, 64, d_in, kernel_size=7, stride=2, padding=3,
+                                bias=False)
+        self.bn1 = split_bn(self.inplanes, d_in)
         self.relu = nn.ReLU(inplace=True)
+
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
         self.layer1 = self._make_layer(block, 1 * 64, 1 * d_in, layers[1], stride=1)
         self.layer2 = self._make_layer(block, 2 * 64, 2 * d_in, layers[2], stride=2)
         self.layer3 = self._make_layer(block, 4 * 64, 4 * d_in, layers[3], stride=2)
         self.layer4 = self._make_layer(block, 8 * 64, 8 * d_in, layers[4], stride=2)
 
-        # for m in self.modules():
-        #     if isinstance(m, nn.Conv2d):
-        #         nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-        #     elif isinstance(m, nn.BatchNorm2d):
-        #         nn.init.constant_(m.weight, 1)
-        #         nn.init.constant_(m.bias, 0)
-
     def _make_layer(self, block, planes, d_out, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                split_conv(self.inplanes, planes * block.expansion, d_out * block.expansion,
-                          kernel_size=1, stride=stride),
-                nn.BatchNorm2d(planes * block.expansion + d_out * block.expansion),
-            )
+        # calling it downsample; technically it is not downsampling but a
+        # linear resampling to adjust number of filter dimensions
+        downsample = nn.Sequential(
+            split_conv(self.inplanes, planes * block.expansion, d_out * block.expansion,
+                       kernel_size=1, stride=stride),
+            split_bn(planes * block.expansion, d_out * block.expansion),
+        )
 
         layers = []
         layers.append(block(self.inplanes, planes, d_out, stride, downsample))
@@ -119,6 +121,7 @@ class ModNet(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
+
         x = self.relu(x)
         x = self.maxpool(x)
 
@@ -126,10 +129,6 @@ class ModNet(nn.Module):
         x = self.layer2(x)
         x = self.layer3(x)
         x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
 
         return x
 
@@ -151,48 +150,67 @@ def modify_resnet():
     return modnet
 
 
-def modify_conv(conv, d_in, d_out):
-    weight = conv.weight
-    cur_out = weight.shape[0]
-    cur_in = weight.shape[1]
-    kernel_shape = weight.shape[2:]
+# these functions are tailored towards modifying resnet weights only.
+# so are not very generic
 
-    # ignore_filters: cur_out, cur_in + d_in, kernel_shape
-    c = torch.zeros((cur_out, d_in) + kernel_shape)
+def init_conv(conv, d_in, d_out):
+    weight = conv.weight
+    old_out = weight.shape[0]
+    old_in = weight.shape[1]
+    kernel_size = weight.shape[2:]
+
+    # ignore_filters: old_out, old_in + d_in, kernel_size
+    c = torch.zeros((old_out, d_in) + kernel_size)
     ig_filter_weight = torch.cat([weight, c], 1)
 
-    # copy_filters: d_out, cur_in + d_in, kernel_shape
-    fan_in = kernel_shape[0] * kernel_shape[1]
-    a = torch.zeros((d_out, cur_in,) + kernel_shape)
+    # copy_filters: d_out, old_in + d_in, kernel_size
+    fan_in = kernel_size[0] * kernel_size[1]
+    a = torch.zeros((d_out, old_in,) + kernel_size)
     b = torch.eye(d_out, d_in).unsqueeze(-1).unsqueeze(-1)
-    b = b.repeat([1, 1, kernel_shape[0], kernel_shape[1]]) / fan_in
+    b = b.repeat([1, 1, kernel_size[0], kernel_size[1]]) / fan_in
     cp_filter_weight = torch.cat([a, b], 1)
 
-    return ig_filter_weight, cp_filter_weight
+    mod_conv = split_conv(old_in + d_in, old_out, d_out, kernel_size=kernel_size, stride=conv.stride, padding=conv.padding)
+
+    mod_conv.copy_filters.weight = cp_filter_weight
+    mod_conv.ignore_filters.weight = ig_filter_weight
+    return mod_conv
 
 
-def modify_bn(bn, d_in, d_out):
-    variance, mean = bn.variance, bn.mean
-    new_variance, new_mean = variance, mean
-    new_bn = nn.BatchNorm2d()
-    return new_bn
+def init_bn(bn, d_in):
+
+    old_in = bn.weight.shape[0]
+    mod_bn = split_bn(old_in, d_in)
+    
+    mod_bn.ignore_bn.running_var = bn.running_var
+    mod_bn.ignore_bn.running_mean = bn.running_mean
+    mod_bn.ignore_bn.weight = bn.weight
+    mod_bn.ignore_bn.bias = bn.bias
+
+    mod_bn.copy_bn.running_var = nn.init.constant(1)
+    mod_bn.copy_bn.running_mean = nn.init.constant(0)
+    mod_bn.copy_bn.weight = nn.init.constant(1)
+    mod_bn.copy_bn.bias = nn.init.constant(0)
+    
+    return mod_bn
 
 
-def modify_bottleneck(bottleneck, d_in):
-    modify_conv(block.conv1)
-    modify_bn(block.bn1)
-    modify_conv(block.conv2)
-    modify_bn(block.bn2)
-    modify_conv(block.conv3)
-    modify_bn(block.bn3)
+def init_bottleneck(block, d_in):
+    mod_block = Bottleneck()
+    block.conv1 = init_conv(block.conv1, d_in)
+    block.bn1 = init_bn(block.bn1, d_in)
+    block.conv2 = init_conv(block.conv2, d_in)
+    block.bn2 = init_bn(block.bn2, d_in)
+    block.conv3 = init_conv(block.conv3, d_in)
+    block.bn3 = init_bn(block.bn3, d_in)
 
     if block.downsample != None:
-        modify_conv(block.downsample)
+        block.downsample = init_conv(block.downsample, d_in)
 
-    return stuff
+    return block
 
 
-def modify_layer(layer, count):
+def init_layer(layer, count):
     for i in range(block_count):
-        modify_bottleneck(layer[i])
+        init_bottleneck(layer[i])
     return stuff

c2a4f371c6809884a540e4d007c2859e1ac275ca
aravind <aravind.nujella@gmail.com>
2018-10-25 06:33:13 +0530

    fixed visualization bugs in val_inference; added utils for converting formats b/w img, inp; network is able to achieve ~0.60 soft_iou loss; pre-cleanup commit;

--- utils.py
+++ utils.py
@@ -8,8 +8,30 @@ def cudify_data(d):
     idx = l[:min(n, 16)]
     return [it[idx].float().cuda() for it in d]
 
+MEAN_PIXEL = np.array(
+    [0.485, 0.456, 0.406], dtype=np.float32).reshape(1, 1, -1)
+STD_PIXEL = np.array(
+    [0.229, 0.224, 0.225], dtype=np.float32).reshape(1, 1, -1)
+
+# single image(wh3) to input format required by pretrained models
+def imgToInp(img):    
+    inp = img.copy() / 255
+    inp -= MEAN_PIXEL
+    inp /= STD_PIXEL
+    inp = np.moveaxis(inp, 2, 0)
+    return inp
+
+# single input(3wh) for pretrained models to RGB image for PIL.Image
+def inpToImg(inp):
+    img = np.moveaxis(inp.copy(), 0, 2)
+    img *= STD_PIXEL
+    img += MEAN_PIXEL
+    img *= 255
+    return img.astype(np.uint8)
+
 # TODO: add load checkpoint??, add validation stats at the end of checkpoint
 #
+
 
 
 class Checkpoint:
